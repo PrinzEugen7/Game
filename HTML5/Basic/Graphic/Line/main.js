d8ef9e34306c6f07a0044fb75a11f5c8297862e7
PrinzEugen7 <nsi05162@pooae.com>
2016-03-12 18:46:32 +0900

    Update main.js

--- HTML5/Basic/Graphic/Line/main.js
+++ HTML5/Basic/Graphic/Line/main.js
@@ -1,11 +1,12 @@
+// 直線を描く (canvas, 始点(x1, y1), 端点(x2, y2), 線色, 線幅
 function drawLine(ctx, x1, y1, x2, y2, color, lw)
 {
-    ctx.beginPath();        // 描画を開始
-    ctx.lineWidth = lw;     // 線の太さ
-    ctx.lineStyle = color;  // 線の色
-    ctx.moveTo(x1, y1);     // 描画開始位置(x1, y1)
-    ctx.lineTo(x2, y2);    // 指定座標(x2, y2)まで線を引く
-    ctx.stroke();           // Canvas上に描画
+    ctx.beginPath();        	// 描画を開始
+    ctx.lineWidth = lw;     	// 線幅
+    ctx.strokeStyle = color; // 線色
+    ctx.moveTo(x1, y1);     	// 直線の始点(x1, y1)
+    ctx.lineTo(x2, y2);    	// 直線の端点(x2, y2)
+    ctx.stroke();           		// Canvas上に描画
 }
 
 function main()
