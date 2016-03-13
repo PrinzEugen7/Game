41e4bd7bbe6f3d25f97115ecb47c2abc1ac07fe7
PrinzEugen7 <nsi05162@pooae.com>
2016-03-13 13:52:23 +0900

    Update main.js

--- HTML5/Basic/Graphic/RectAngle/main.js
+++ HTML5/Basic/Graphic/RectAngle/main.js
@@ -1,10 +1,10 @@
 // 直線を描く (canvas, 左上端座標(x1, y1), 幅, 高さ, 色
 function drawLine(ctx, x, y, w, h, color)
 {
-    ctx.beginPath();        	// 描画を開始
-    ctx.fillStyle = color; // 線色
-    ctx.fillRect(x, y, w, h);     	// 四角形の始点(x1, y1), 幅w, 高さh
-    ctx.stroke();           		// Canvas上に描画
+    ctx.beginPath();        	// パスの初期化
+    ctx.fillStyle = color;      // 線色
+    ctx.fillRect(x, y, w, h);   // 四角形の始点(x1, y1), 幅w, 高さh
+    ctx.closePath();            // パスの終了
 }
 
 function main()
