d53de1a2d9456f162e137697644e29bc44ff97ab
PrinzEugen7 <nsi05162@pooae.com>
2016-08-04 22:09:59 +0900

    Update maze.c

--- C/Basic/maze.c
+++ C/Basic/maze.c
@@ -16,24 +16,23 @@ int main()
     gx;
     gy=y-2;
 
-    image = (int *) malloc( /*適当な式*/ * sizeof(/*適当な型*/) );
+    image = (int *) malloc( x*y * sizeof(int) );
     if (image==NULL){ printf("メモリ不足エラー\n"); return -1; }
 
-    /* 全てを見えない壁(1)にする */
+    // 全てを見えない壁(1)にする 
     for (j=0; j<y; j++){ for (i=0; i<x; i++) image[x*j+i]=1; }
-    /*奇数番地は床(0)にする*/
+    // 奇数番地は床(0)にする
     for (j=1; j<y-1; j++){ for (i=1; i<x-1; i++) 
         if ( i%2!=0|| j%2!=0) image[x*j+i]=0;
     }
-    /*迷路生成（棒倒し法）*/
+    // 迷路生成（棒倒し法）
     for (j=2; j<y-2; j+=2){ for (i=2; i<x-2; i+=2) 
         if ( rand()%2 ) image[x*(j+1)+i]=1; else image[x*j+i+1]=1;
     }
 
     printf("迷路ゲームスタート！\n");
-    /*ここから無限ループ*/
-    while(){
-        /*メモリ内にある迷路を表示*/
+    while(1){
+        // メモリ内の迷路を表示
         for (j=0; j<y; j++){ 
           for (i=0; i<x; i++){
             if (image[x*j+i]==3) printf("#");
@@ -43,31 +42,28 @@ int main()
             
           }  printf("\n");
         }
-        printf("** ゴールマーク[G]マークを目指せ！ **\n");
-        printf("Command (2[down],4[left],6[right],8[up])>＞");
+        printf("【コマンド】 2:下 4:左 6:右 8:上");
         scanf("%d",&command);
         if (command==0){ flag=-1; break;}
         if (command==2 && hy<y && image[x*(hy+1)+hx]%2!=1) hy+=1;
         if (command==8 && hy>0 && image[x*(hy-1)+hx]%2!=1) hy-=1;
         if (command==4 && hx>0 && image[x*hy+(hx-1)]%2!=1) hx-=1;
         if (command==6 && hx<x && image[x*hy+(hx+1)]%2!=1) hx+=1;
-        /*自分の周囲だけ見えるようにする（見える壁(3)にする）*/
+        // 自分の周囲だけ見えるようにする（見える壁(3)にする）
         if (image[x*(hy+1)+hx]==1) image[x*(hy+1)+hx]=3 ;
         if (image[x*(hy-1)+hx]==1) image[x*(hy-1)+hx]=3;
         if (image[x*hy+(hx-1)]==1) image[x*hy+(hx-1)]=3;
         if (image[x*hy+(hx+1)]==1) image[x*hy+(hx+1)]=3;
-        if (hx==gx && hy==gy){flag=1; break;} /*ゴール到着！*/
+        if (hx==gx && hy==gy){flag=1; break;} // ゴール到着
     }
-    /*ここまでが無限ループ*/
 
     if (flag==1){
-        printf("ゴール到着\nおめでとうございます！\n");
         end = clock();
-         printf("%.2f秒かかりました\n",
-              (double)(end-start)/CLOCKS_PER_SEC);    }
-    else printf("ゲームを中断しました．\n");
+        printf("ゲームクリア\n プレイ時間は%.2f秒です\n",(double)(end-start)/CLOCKS_PER_SEC);
+    }
+    else printf("ゲームを中断します\n");
 
-    /*メモリ内にある迷路をすべて表示する*/
+    // メモリ内にある迷路をすべて表示する
     for (j=0; j<y; j++){ 
         for (i=0; i<x; i++){
             if (image[x*j+i]%2==1) printf("#");
