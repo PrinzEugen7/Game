5ce7aee592cc414d98464bb67684887d147f7cae
PrinzEugen7 <nsi05162@pooae.com>
2017-01-12 23:36:15 +0900

    Create main.py

---
+++ Python/PyGame/Basic/WindowTitle/main.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+import pygame
+from pygame.locals import *
+import sys
+
+
+def main():
+    pygame.init()                                   # Pygameの初期化
+    screen = pygame.display.set_mode((600, 500))    # 大きさ600*500の画面を生成
+    pygame.display.set_caption("GAME")              # タイトルバーに表示する文字
+
+    while (1):
+        screen.fill((0,0,0))        # 画面を黒色(#000000)に塗りつぶし
+        pygame.display.update()     # 画面を更新
+        # イベント処理
+        for event in pygame.event.get():
+            if event.type == QUIT:  # 閉じるボタンが押されたら終了
+                pygame.quit()       # Pygameの終了(画面閉じられる)
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
